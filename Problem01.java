0775567f098975298446902f2775423853e58522
Thalia La Pommeray <72656510+teealia@users.noreply.github.com>
2022-04-13 21:05:43 -0400

    Add files via upload

---
+++ Problem01.java
@@ -0,0 +1,126 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class Problem01 {
+    public static volatile ConcurrentLinkedQueue<present> LinkedList = new ConcurrentLinkedQueue<>();
+    public static HashSet <present> presents = new HashSet<>();
+    public static volatile int currentId = 0;
+    public static volatile int numThankYous;
+    public static volatile int altTask;
+//    public static present curPresent;
+//    public int ID;
+//    public static int hasThankYou;
+
+    public static class myrun implements Runnable {
+
+        @Override
+        public synchronized void run() {
+            // System.out.println("entering thread");
+
+            if (altTask==1){ // adding gifts to the ordered chain
+                present temp = new present(currentId);
+                if(presents.contains( temp )){
+                    LinkedList.add(temp);
+                    presents.remove(temp);
+                //    System.out.print(LinkedList);
+
+                }
+                altTask = 2;
+            }
+
+            if (altTask==2){ // writing “Thank you” cards
+                present temp = new present(currentId);
+                if(!LinkedList.isEmpty()){
+                    LinkedList.remove(temp);
+                    numThankYous++;
+                    currentId++;
+               //     System.out.print(LinkedList);
+                }
+                altTask = 1;
+            }
+
+            }
+        }
+
+    public static void main(String[] args){
+        myrun tasks = new myrun();
+        Thread [] servants = new Thread[4];
+        Scanner sc = new Scanner(System.in);
+
+
+        // present curPresent;
+        int n;
+        int t = 0;
+        altTask = 1;
+        numThankYous = 0;
+
+        System.out.println("How many Presents?\n");
+        n = sc.nextInt(); // number of Guests
+
+        System.out.println("Servants are on it!");
+        for (int i=0; i<n; i++){
+            presents.add(new present(currentId));
+            currentId++;
+        }
+
+        final long startTime = System.currentTimeMillis();
+
+        currentId = 0;
+        while (numThankYous<n){
+            servants[t] = new Thread(tasks);
+            servants[t].start();
+
+            if (t==3) t=0;
+            else t++;
+        }
+
+
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Servants done! (: wanna know how well they did?");
+        System.out.println("Here's whats left in our bag of unordered presents and chain of ordered presents");
+        System.out.print(presents);
+        System.out.print(LinkedList);
+        System.out.println();
+        System.out.printf("Time taken: %f seconds\n", (float)(endTime-startTime)/1000);
+        System.out.printf("Number of Thank you card written: %d\n", numThankYous);
+
+
+
+    }
+    public static class present {
+        int ID;
+        int hasThankYou = 0;
+
+        public present(int id){
+            this.ID = id;
+        }
+
+        @Override
+        public int hashCode(){
+            int hash = 22;
+            hash = 32 * hash + this.ID;
+            return hash;
+        }
+
+        @Override
+        public boolean equals( Object obj){
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            present other = (present) obj;
+            if (hasThankYou != other.hasThankYou)
+                return false;
+            return ID == other.ID;
+        }
+
+        public String toString(){
+            return "id: "+ID;
+    }
+
+}
+}
